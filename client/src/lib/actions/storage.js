168d8550b4ae9bbdc27cc32d30cef5ed0e6a6a7b
Sandy Instructor <sandyludosky@gmail.com>
2021-03-28 00:48:02 +0100

    edit action types and creators

--- client/src/lib/actions/storage.js
+++ client/src/lib/actions/storage.js
@@ -1,22 +1,26 @@
 import {
-  SET_VALUE_ERROR,
   SET_VALUE_SUCCESS,
-  SET_VALUE_PENDING,
   GET_VALUE_SUCCESS,
-  GET_VALUE_ERROR,
-  GET_VALUE_PENDING,
+  TRANSACTION_ERROR,
+  TRANSACTION_PENDING,
 } from "./types";
 
+const transactionPending = () => {
+  return {
+    type: TRANSACTION_PENDING,
+    payload: { isPending: true },
+  };
+};
+const transactionError = (err) => {
+  return {
+    type: TRANSACTION_ERROR,
+    payload: { error: err },
+  };
+};
 const getValueSuccess = (value) => {
   return {
     type: GET_VALUE_SUCCESS,
     payload: { storageValue: value, isPending: false },
-  };
-};
-const setValuePending = () => {
-  return {
-    type: SET_VALUE_PENDING,
-    payload: { isPending: true },
   };
 };
 const setValueSuccess = (value) => {
@@ -25,28 +29,10 @@ const setValueSuccess = (value) => {
     payload: { isPending: value },
   };
 };
-const getValueError = (err) => {
-  return {
-    type: GET_VALUE_ERROR,
-    payload: { error: err },
-  };
-};
-const getValuePending = () => {
-  return {
-    type: GET_VALUE_PENDING,
-    payload: { isPending: true },
-  };
-};
-const setValueError = (err) => {
-  return {
-    type: SET_VALUE_ERROR,
-    payload: { error: err },
-  };
-};
 
 export const setValue = (value) => {
   return (dispatch, _, { instances: { SimpleStorage }, admin }) => {
-    dispatch(setValuePending());
+    dispatch(transactionPending());
     SimpleStorage.methods
       .set(value)
       .send({ from: admin })
@@ -54,16 +40,16 @@ export const setValue = (value) => {
         dispatch(setValueSuccess(!result.status));
         dispatch(getValue());
       })
-      .catch(setValueError);
+      .catch(transactionError);
   };
 };
 export const getValue = () => {
   return (dispatch, _, { instances: { SimpleStorage } }) => {
-    dispatch(getValuePending());
+    dispatch(transactionPending());
     SimpleStorage.methods
       .get()
       .call()
       .then((result) => dispatch(getValueSuccess(result)))
-      .catch(getValueError);
+      .catch(transactionError);
   };
 };
--- client/src/lib/actions/types.js
+++ client/src/lib/actions/types.js
@@ -1,10 +1,8 @@
 //types contract
 export const SET_VALUE_SUCCESS = "SET_VALUE_SUCCESS";
-export const SET_VALUE_ERROR = "SET_VALUE_ERROR";
-export const SET_VALUE_PENDING = "SET_VALUE_PENDING";
 export const GET_VALUE_SUCCESS = "GET_VALUE_SUCCESS";
-export const GET_VALUE_ERROR = "GET_VALUE_ERROR";
-export const GET_VALUE_PENDING = "GET_VALUE_PENDING";
+export const TRANSACTION_ERROR = "TRANSACTION_ERROR";
+export const TRANSACTION_PENDING = "TRANSACTION__PENDING";
 
 export const LOG_EVENT = "LOG_EVENT";
 export const DEPLOY = "DEPLOY";
